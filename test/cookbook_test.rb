ff48847ccd6795355ef4773776b6d21b911a121e
Connor Ferguson <cpfergus1@gmail.com>
2020-09-23 09:54:09 -0600

    Initialized cookbook class

--- test/cookbook_test.rb
+++ test/cookbook_test.rb
@@ -1,13 +1,13 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/ingredient'
+require './lib/cook_book'
 require './lib/recipe'
 
 class CookBookTest < Minitest::Test
 
 def test_cook_book_has_attributes
   cookbook = CookBook.new
-  assert_equal [], CookBook.recipes
+  assert_equal [], cookbook.recipes
 end
 
 # def test_cookbook_can_add_recipe
@@ -18,3 +18,4 @@ end
 # cookbook.add_recipe(recipe2)
 # assert_equal [recipe1, recipe2], cookbook.recipes
 # end
+end
