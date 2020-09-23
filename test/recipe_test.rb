e093ec3aebf2631d98a293909a90464a271d2bf9
Connor Ferguson <cpfergus1@gmail.com>
2020-09-23 09:43:23 -0600

    Implemented tests for ingredients required and ingredients array

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -11,13 +11,27 @@ class RecipeTest < Minitest::Test
     expected = Hash.new(0)
     assert_equal expected, recipe1.ingredients_required
   end
-# ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
-# ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
-# recipe1.add_ingredient(ingredient1, 2)
-# recipe1.add_ingredient(ingredient1, 4)
-# recipe1.add_ingredient(ingredient2, 8)
-# recipe1.ingredients_required
-# recipe1.ingredients
 
+  def test_recipe_can_add_ingredients
+    recipe1 = Recipe.new("Mac and Cheese")
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient1, 4)
+    recipe1.add_ingredient(ingredient2, 8)
+    expected = {ingredient1 => 6, ingredient => 8}
+    assert_equal expected, recipe1.ingredients_required
+  end
+
+  def test_recipe_can_list_ingredients_names
+    recipe1 = Recipe.new("Mac and Cheese")
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient1, 4)
+    recipe1.add_ingredient(ingredient2, 8)
+    expected [ingredient1, ingredient2]
+    assert_equal expected, recipe1.ingredients
+  end
 
 end
