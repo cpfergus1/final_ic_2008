99d6b8f1ebeebc2dbc6563119d0853925f8775c9
Connor Ferguson <cpfergus1@gmail.com>
2020-09-23 09:13:25 -0600

    Update Attribute test with assert clause

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -5,11 +5,12 @@ require './lib/pantry'
 
 class PantryTest < Minitest::Test
 
-def test_pantry_has_attributes
-pantry = Pantry.new
-pantry.stock
+  def test_pantry_has_attributes
+    pantry = Pantry.new
+    expected = Hash.new(0)
+    assert_equal expected, pantry.stock
+  end
 end
-
 # ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
 # ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
 # pantry.stock
