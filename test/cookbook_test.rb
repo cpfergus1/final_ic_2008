3e37738d5552c1f693099e44be4ad7d0934d0082
Connor Ferguson <cpfergus1@gmail.com>
2020-09-23 10:02:30 -0600

    Implemented test for listing ingredient names

--- test/cookbook_test.rb
+++ test/cookbook_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/cook_book'
 require './lib/recipe'
+require './lib/ingredient'
 
 class CookBookTest < Minitest::Test
 
@@ -19,4 +20,22 @@ class CookBookTest < Minitest::Test
     cookbook.add_recipe(recipe2)
     assert_equal [recipe1, recipe2], cookbook.recipes
   end
+
+def test_cookbook_can_list_ingredient_names
+cookbook = CookBook.new
+ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+recipe1 = Recipe.new("Mac and Cheese")
+recipe1.add_ingredient(ingredient1, 2)
+recipe1.add_ingredient(ingredient2, 8)
+ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+recipe2 = Recipe.new("Cheese Burger")
+recipe2.add_ingredient(ingredient1, 2)
+recipe2.add_ingredient(ingredient3, 4)
+recipe2.add_ingredient(ingredient4, 1)
+cookbook.add_recipe(recipe1)
+cookbook.add_recipe(recipe2)
+cookbook.ingredients
+end
 end
