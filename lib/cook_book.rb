2e34de582b539799b297ba1c85f9a8c28a72cf68
Connor Ferguson <cpfergus1@gmail.com>
2020-09-23 10:11:05 -0600

    Implemented method for listing ingredient names in cookbook class

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -8,4 +8,14 @@ attr_reader :recipes
   def add_recipe(recipe)
     @recipes << recipe
   end
+
+  def ingredients
+    ingredients = []
+    @recipes.each do |recipe|
+      recipe.ingredients.each do |ingredient|
+        ingredients << ingredient.name unless ingredients.include?(ingredient.name)
+      end
+    end
+    ingredients
+  end
 end
