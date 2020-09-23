850af056878ff3fc7d9631dac2b372df1678e668
Connor Ferguson <cpfergus1@gmail.com>
2020-09-23 09:54:52 -0600

    Implemented add recipe mehtod for cookbook

--- test/cookbook_test.rb
+++ test/cookbook_test.rb
@@ -5,17 +5,17 @@ require './lib/recipe'
 
 class CookBookTest < Minitest::Test
 
-def test_cook_book_has_attributes
-  cookbook = CookBook.new
-  assert_equal [], cookbook.recipes
-end
+  def test_cook_book_has_attributes
+    cookbook = CookBook.new
+    assert_equal [], cookbook.recipes
+  end
 
-# def test_cookbook_can_add_recipe
-# recipe1 = Recipe.new("Mac and Cheese")
-# recipe2 = Recipe.new("Cheese Burger")
-# cookbook = CookBook.new
-# cookbook.add_recipe(recipe1)
-# cookbook.add_recipe(recipe2)
-# assert_equal [recipe1, recipe2], cookbook.recipes
-# end
+  def test_cookbook_can_add_recipe
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe2 = Recipe.new("Cheese Burger")
+    cookbook = CookBook.new
+    cookbook.add_recipe(recipe1)
+    cookbook.add_recipe(recipe2)
+    assert_equal [recipe1, recipe2], cookbook.recipes
+  end
 end
