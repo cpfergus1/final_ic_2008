5e62bf09207f23cf9ebc2086b7a2fdcaac2f6857
Connor Ferguson <cpfergus1@gmail.com>
2020-09-23 09:22:52 -0600

    implemented test for restock items

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -19,14 +19,20 @@ class PantryTest < Minitest::Test
     assert_equal expected, pantry.stock
     assert_equal 0, pantry.stock_check(ingredient1)
   end
-end
 
-# ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
-# ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
-# pantry.stock
-# pantry.stock_check(ingredient1)
-# pantry.restock(ingredient1, 5)
-# pantry.restock(ingredient1, 10)
-# pantry.stock_check(ingredient1)
-# pantry.restock(ingredient2, 7)
-# pantry.stock_check(ingredient2)
+  def test_pantry_can_stock_items
+    pantry = Pantry.new
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+    expected = Hash.new(0)
+    assert_equal expected, pantry.stock
+    assert_equal 0, pantry.stock_check(ingredient1)
+    pantry.restock(ingredient1, 5)
+    pantry.restock(ingredient1, 10)
+    expected = {ingredient1 => 15}
+    assert_equal expected, pantry.stock_check(ingredient1)
+    pantry.restock(ingredient2, 7)
+    expected = {ingredient1 => 15, ingredient2 => 7}
+    assert_equal expected, pantry.stock_check(ingredient2)
+  end
+end
